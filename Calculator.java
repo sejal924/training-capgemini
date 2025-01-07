581e5ccda93646aa2c3c29d52368b1d29acb40a5
sejal924 <sejalchouhan24@gmail.com>
2025-01-07 13:27:49 +0530

    add uc1_addition

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,15 @@
-public class Calculator
+import java.util.Scanner;
+public class Addition
 {
-  public static void main(String [] args)
-  {
-    System.out.println("Welcome to calculator");
-  }
+     public static void main(String [] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("enter the first number:");
+      int num1 = sc.nextInt();
+      System.out.print("enter the second number:");
+      int num2 = sc.nextInt();
+      System.out.print("Adding two number:");
+      System.out.println(num1+num2);
+     }
+
 }
