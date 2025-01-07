c11c1d5d12eff12f5aa3f53320ba858c67195933
sejal924 <sejalchouhan24@gmail.com>
2025-01-07 13:46:27 +0530

    Multiplication uc3_multiplication

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Subtraction
+public class Multiplication
 {
     public static void main(String [] args)
     {
@@ -8,7 +8,7 @@ public class Subtraction
        int num1 = sc.nextInt();
        System.out.print("enter the second number: ");
        int num2 = sc.nextInt();
-       System.out.print("Subtraction of two number: ");
-       System.out.print(num1-num2);
+       System.out.print("Multiplication of two number: ");
+       System.out.print(num1*num2);
     }
 }
