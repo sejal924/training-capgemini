40c8eb2875cd2de85f1aae130dac24d2ef87cbc0
sejal924 <sejalchouhan24@gmail.com>
2025-01-07 13:37:37 +0530

    subtract uc2_subtraction

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-public class Addition
+public class Subtraction
 {
-     public static void main(String [] args)
+    public static void main(String [] args)
     {
-      Scanner sc = new Scanner(System.in);
-      System.out.print("enter the first number:");
-      int num1 = sc.nextInt();
-      System.out.print("enter the second number:");
-      int num2 = sc.nextInt();
-      System.out.print("Adding two number:");
-      System.out.println(num1+num2);
-     }
-
+       Scanner sc= new Scanner(System.in);
+       System.out.print("enter the first number: ");
+       int num1 = sc.nextInt();
+       System.out.print("enter the second number: ");
+       int num2 = sc.nextInt();
+       System.out.print("Subtraction of two number: ");
+       System.out.print(num1-num2);
+    }
 }
